bdbde5f2538c4c9909545c37512d6f96fc91a564
bieners <bienmarlon.qm@gmail.com>
2025-02-16 19:21:37 +0800

    New route

--- pcfinds/routes/web.php
+++ pcfinds/routes/web.php
@@ -1,11 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegistrationControl;
+use App\Http\Controllers\SignInController;
 
+# Route for the home page
 Route::get('/', function () {
     return view('welcome');
 });
 
+# Route for the products page
 Route::get('/products_page', function () {
     return view('products_page');
 });
+
+# Route for the sign-up form
+Route::get('/sign-up', function () {
+    return view('paging.sign_up');
+})->name('sign-up');
+
+# Route for form sign-up submission
+Route::post('/sign-up', [RegistrationControl::class, 'account_register'])->name('account_register_route');
+
+// Sign In
+
+# Route for the sign-in form
+Route::get('/sign-in', function () {
+    return view('paging.sign_in');
+})->name('sign-in');
+
+# Route for form sign-in submission
+Route::post('/sign-in', [SignInController::class, 'account_sign_in'])->name('account_sign_in_route');
