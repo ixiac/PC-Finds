5a8295fd87baefa7418bbf2abef47e9bf199b456
bieners <bienmarlon.qm@gmail.com>
2025-02-18 12:54:00 +0800

    Config

--- pcfinds/config/auth.php
+++ pcfinds/config/auth.php
@@ -61,8 +61,9 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+            'driver' => 'database',
+            'table'=> 'account',
+            'model' => env('AUTH_MODEL', App\Models\Account::class),
         ],
 
         // 'users' => [
